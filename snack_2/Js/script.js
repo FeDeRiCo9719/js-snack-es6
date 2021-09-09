254376b35b0b84ea4ff50d3a3447b94f8e6fc0aa
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-09-09 17:32:20 +0200

    loop punteggi random

--- snack_2/Js/script.js
+++ snack_2/Js/script.js
@@ -7,5 +7,58 @@ Punti fatti e falli subiti.
 Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 */
 
+/*
+FUNCTION
+*/ 
+const numRandom = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);   // codice su una riga sola
+/*
+const numRandom = (min, max) => {
+    Math.floor(Math.random() * (max - min + 1) + min);
+}
+*/
 
+
+// 1. Creare un array di oggetti di squadre di calcio.
+// - Ogni squadra avrà diverse proprietà: nome, goal fatti, goal subiti.
+// - Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+const Teams = [
+    {
+        nome: 'team_A',
+        goalFatti: 0,
+        goalSubiti: 0
+    },
+
+    {
+        nome: 'team_B',
+        goalFatti: 0,
+        goalSubiti: 0
+    },
+
+    {
+        nome: 'team_C',
+        goalFatti: 0,
+        goalSubiti: 0
+    },
+
+    {
+        nome: 'team_D',
+        goalFatti: 0,
+        goalSubiti: 0
+    }
+];
+
+
+// 2. Generare numeri random al posto degli 0 nelle proprietà: goal fatti e goal subiti.
+for ( let i=0; i < Teams.length; i++ ) {
+
+    Teams[i].goalFatti= numRandom(i, 5);
+    Teams[i].goalSubiti= numRandom(0, 5);
+
+    console.log( "goal Fatti = " + Teams[i].goalFatti );
+    console.log( "goal Subiti = " + Teams[i].goalSubiti );
+    console.log("");
+}
+
+
+// 3. Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e goal subiti e stampiamo tutto in console.
 
