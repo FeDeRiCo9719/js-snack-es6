5343409e14addd9899c567c48224862c8ac08adf
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-09-14 15:50:08 +0200

    arrow function

--- snack_3/Js/script.js
+++ snack_3/Js/script.js
@@ -6,32 +6,32 @@ Usiamo i nuovi metodi degli array foreach o filter.
 */
 
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b)
-function myFunction( arr, num1, num2 ) {
-
-    const Array = [];
-
-    arr.forEach( function (item,index) {
-        if (index > num1 && index < num2) {
-            Array.push(item);
-        }
-    });
-    return Array;
-}
-
-// function myFunction_2( arr, num1, num2 ) {
+// function myFunction( arr, num1, num2 ) {
 
 //     const Array = [];
 
-//     arr.forEach(
-//         (item, index) => {
-//             if (index > num1 && index < num2) {
-//                 Array.push(item);
-//             }
+//     arr.forEach( function (item,index) {
+//         if (index > num1 && index < num2) {
+//             Array.push(item);
 //         }
-//     );
-
+//     });
 //     return Array;
 // }
+
+function myFunction_2( arr, num1, num2 ) {
+
+    const Array = [];
+
+    arr.forEach(
+        (item, index) => {
+            if (index > num1 && index < num2) {
+                Array.push(item);
+            }
+        }
+    );
+
+    return Array;
+}
 
 // const myFunction_3 = ( arr, num1, num2 ) => {
 //     return arr.filter(
@@ -51,7 +51,7 @@ let Number_B = 9;
 const array1 = ['a', 'b', 'c','d', 'e', 'f','g', 'h', 'i', 'l'];
 
 // // 2. Usiamo i nuovi metodi degli array foreach o filter
-const NewArray = myFunction(array1, Number_A, Number_B );
+const NewArray = myFunction_2(array1, Number_A, Number_B );
 console.log(NewArray);
 
 
